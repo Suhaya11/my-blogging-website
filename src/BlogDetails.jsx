0cc29388516f8f895032bfac00e1d3bf2b76095a
suhaya11 <sulaimanhaladukadigawakibiya@gmail.com>
2026-01-11 01:29:18 +0100

    Api
     today i added jsonPlacehoder api to my blogging website becouse i want added it to netlify so that i can share its link with other friends and relative ALLHAMDULILL

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,26 +1,23 @@
 import useFetch from "./useFetch";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import MyBlogs from "./MyBlogs";
+
 import ReactionsOnBlog from "./ReactionsOnBlog";
 
 const BlogDetails = () => {
   let { id } = useParams();
 
-<<<<<<< HEAD
+
   let blogUrl = `https://jsonplaceholder.typicode.com/posts/${id}`;
-=======
-  let blogUrl = `https://suha andyadb-a550.restdb.io/rest/blogs/${id}`;
->>>>>>> 23cef3199c0d30ccd4c0e5dcd45a674eb5ff01d5
+
+  
+
 
   let { data: blog } = useFetch(blogUrl, {
     method: "GET",
     Headers: {
-<<<<<<< HEAD
+
      
-=======
-      "x-apikey": "",
->>>>>>> 23cef3199c0d30ccd4c0e5dcd45a674eb5ff01d5
+
       "Content-type": "application/json",
     },
   });
